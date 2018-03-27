4090d70991b718ed5fd177874fe23bb22749e7f1
prettykara <prettykara@192.168.0.100>
2018-03-27 16:04:45 +0900

    war 파일로 package

--- src/net/slipp/support/MyValidatorFactory.java
+++ src/net/slipp/support/MyValidatorFactory.java
@@ -4,11 +4,8 @@ import javax.validation.Validation;
 import javax.validation.Validator;
 import javax.validation.ValidatorFactory;
 
-import org.junit.BeforeClass;
-
 public class MyValidatorFactory {
 
-	@BeforeClass
 	public static Validator createValidator() {
 		ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
 		return factory.getValidator();
